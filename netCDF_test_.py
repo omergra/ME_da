3e5807f7bdfd09c92c570153808a505cc719ef4e
Omer g <omer.gra@gmail.com>
2018-07-08 17:56:45 +0300

    a

--- netCDF_test_.py
+++ netCDF_test_.py
@@ -9,8 +9,8 @@ import matplotlib.pyplot as plt
 from netCDF4 import Dataset
 import xarray as xr
 import scipy as sp
-import xrft
-
+import scipy.signal as signal
+import pandas as pd
 
 def find_th(ds, type='filt'):
     th_per_subj = dict()
@@ -67,37 +67,126 @@ def apply_rms(data, window_size=1000):
     # return RMS
     return np.sqrt(np.convolve(sqr_data, window, 'same'))
 
+def apply_savgol(data):
+    savgol_list = []
+    for i in data.coords['electrode'].values:
+        savgol_list.append(signal.savgol_filter(data.sel(electrode=i).values, window_length=10000,
+                                                polyorder=0, mode='nearest'))
+    return savgol_list
+
 def create_rms_data(data):
     data_list = []
     for elec in data.coords['electrode'].values:
         data_list.append(apply_rms(data.sel(electrode=elec)))
-    return np.stack(data_list, axis=1)
+    return data_list
 
 # test for one subject
 def data_analysis(da):
-    da_diff_1 = differential_channel(da.sel(subject=3, electrode=slice(8, 15)), 12)
-    da_diff_2 = differential_channel(da.sel(subject=3, electrode=slice(0, 7)), 5)
+    # create differential data
+    da_diff_1 = differential_channel(da.sel(subject=5, electrode=slice(8, 15)), 12)
+    da_diff_2 = differential_channel(da.sel(subject=5, electrode=slice(0, 7)), 5)
     # data 1 diff will be the final DataArray
-    da_diff_1.combine_first(da_diff_2)
+    comb_arr = da_diff_1.combine_first(da_diff_2)
+    # removing mean
+    array_no_mean = (comb_arr.values.T-np.mean(comb_arr.values, axis=1)).T
+    # rectifying data
+    rectified = np.abs(array_no_mean)
+    comb_arr = xr.DataArray(rectified, dims=('electrode', 'time'))
     # rms data
-    rms_da = create_rms_data(da_diff_1)
-    # finding peaks
-    return rms_da
+    #rms_da = create_rms_data(comb_arr)
+    low_pass = 2/1500
+    b2, a2 = sp.signal.butter(3, low_pass, btype='lowpass')
+    env = sp.signal.filtfilt(b2, a2, rectified)
 
-def find_peaks(da, prominence=1.2, width=300):
+    #sav_arr = apply_savgol(comb_arr)
+    # finding peaks
+    env_mf = match_filter(data=env, width=1000, sig=300)
+    find_peaks_cwt(env)
+    #peaks = find_peaks(env_mf)
+    #peaks = find_peaks_cwt(env)
+
+    return env, peaks, rectified, env_mf
+
+def find_peaks(data, prominence=10, width=(500, 2000), height=5):
     # this function uses scipy find peaks to detect areas of interest
-    peaks = [sp.signal.find_peaks(rms_ch, prominence=prominence, width=width) for rms_ch in rms_data]
-    #self.peaks = [signal.find_peaks_cwt(rms_ch, widths=[300, 500, 1000], min_snr=1.2) for rms_ch in self.filtered_data]
+
+    return [signal.find_peaks(ch, prominence=prominence, width=width, height=5) for ch in data]
+
+def find_peaks_cwt(data):
+    return [sp.signal.find_peaks_cwt(ch, min_snr=10) for ch in data]
+
+def unify_peaks(peaks, tol=0.5, fps=3000):
+    tol = tol*fps
+    flat_list = [item for sublist in peaks for item in sublist[0]]
+    peaks_only = [list(row[0]) for row in peaks]
+    flat_list.sort()
+    clean_list = []
+    prominance = []
+    width = []
+    for idx, item in enumerate(flat_list):
+        for other in flat_list[idx+1:-1]:
+            if check_in_range(low=item-tol, high=item+tol, number=other):
+                flat_list.remove(other)
+                idx = index_2d(peaks_only, other)
+            else:
+                idx = index_2d(peaks_only, other)
+                feature = get_peaks_data(peaks[idx[0]][1], idx[1])
+                if item not in clean_list:
+                    clean_list.append(item)
+                    prominance.append(feature[0])
+                    width.append(feature[1])
+                break
+    return clean_list, prominance, width
+
+def index_2d(myList, v):
+    for i, x in enumerate(myList):
+        if v in x:
+            return (i, x.index(v))
+
+def get_peaks_data(peaks_dict, idx):
+
+    prom = peaks_dict['prominences'][idx]
+    width = peaks_dict['widths'][idx]
+    return prom, width
+
+def remove_entery_in_dict():
+    pass
+
+def match_filter(data, width, sig):
+
+    return [gaussian_filter(ch, sig, width) for ch in data]
+
+def check_in_range(low, high, number):
+
+    return low <= number <= high
+
+def gaussian_filter(data, sig, width):
+
+    window = sp.signal.general_gaussian(width, p=0.5, sig=sig)
+    filtered = signal.fftconvolve(window, data)
+    filtered = (np.average(data) / np.average(filtered)) * filtered
+    filtered = np.roll(filtered, int(-np.floor(width/2)))
+    return filtered
 
 
 if __name__ == '__main__':
 
-    #pth = 'F:\Data\Electrodes\MircoExpressions\ExperimentME_data_filt_comb.nc'
     pth = 'F:\Data\Electrodes\MircoExpressions\ExperimentME_data_filt_comb.nc'
     ds = xr.open_dataset(pth)
     da = ds.__xarray_dataarray_variable__
-    rms_npa = data_analysis(da)
-    rms_da = xr.DataArray(rms_npa, dims=('time', 'electrode'))
-    time = ds.__xarray_dataarray_variable__.coords['time'].values
+    output = data_analysis(da)
+    env = output[0]
+    peaks = output[1]
+    rectified = output[2]
+    mf = output[3]
+    uni_list = unify_peaks(peaks)
+    #plt.plot(rectified[0])
+    plt.plot(env[0], 'b')
+    plt.plot(mf[0], 'g')
+    plt.plot(uni_list[0], env[0, uni_list[0]], 'r+')
+    dat_dict = {'Peak': uni_list[0], 'Prominence': uni_list[1], 'width': uni_list[2]}
+    dat = pd.DataFrame(dat_dict)
+    #dat.to_csv('subject_5.csv')
     plt.show()
-    #rms_da.to_netcdf(r'F:\Data\Electrodes\MircoExpressions\rms_1000.nc')
+
+    gaussian_filter(rectified, 300)
